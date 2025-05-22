59a28988f92a42e71780d33c9666f99da1904815
sabba1010 <sabbahossain@gmaol.com>
2025-05-22 14:14:04 +0600

    Update Planrs

--- index.js
+++ index.js
@@ -1,90 +1,3 @@
-// const express = require('express');
-// const cors = require('cors');
-// require('dotenv').config();
-// const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
-// const app = express();
-// const port = process.env.PORT || 3000;
-
-// app.use(cors());
-// app.use(express.json());
-
-
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.djvkmk5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-
-// const plantsCollection = client.db('plantsDB').collection('plants')
-// const userCollection = 
-
-// app.get('/plants',  async(req, res)  =>{
-//     // const cursor = plantsCollection.find();
-//     // const result = await cursor.toArray();
-//     const result = await plantsCollection.find().toArray();
-//     res.send(result);
-// })
-
-// app.get('/plants/:id', async (req, res) =>{
-//     const id = req.params.id;
-//     const query = {_id: new ObjectId(id) }
-//     const result = await plantsCollection.findOne(query);
-//     res.send(result);
-// })
-
-//   app.post('/plants', async(req, res) =>{
-//   const newPlants = req.body;
-//   console.log(newPlants);
-//   const result = await plantsCollection.insertOne(newPlants);
-//   res.send(result);
-//   })
-
-
-//   app.delete('/plants/:id', async(req, res) =>{
-//       const id = req.params.id;
-//       const query = {_id: new ObjectId(id)}
-//       const result = await plantsCollection.deleteOne(query);
-//       res.send(result);
-//     })
-
-
-//   //  user related apis
-
-
-
-    
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     // await client.close();
-//   }
-// }
-// run().catch(console.dir);
-
-
-
-// app.get('/',(req, res) =>{
-//     res.send('Tree Plant Server Getting Ready.')
-// });
-
-
-// app.listen(port, () =>{
-//     console.log(`Tree Plant Server Runing ${port}`);
-    
-// });
-
 
 const express = require('express');
 const cors = require('cors');
@@ -134,6 +47,13 @@ async function run() {
       const result = await plantsCollection.insertOne(newPlant);
       res.send(result);
     });
+
+app.put('/plants/:id',  async(req, res) =>{
+  const id  = req.params.id;
+  const filter = { _id: new ObjectId(id)}
+  const options = {upsert: true};
+})
+
 
     app.delete('/plants/:id', async (req, res) => {
       const id = req.params.id;
